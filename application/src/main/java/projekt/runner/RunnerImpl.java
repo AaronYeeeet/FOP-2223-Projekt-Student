3df9fcf85dbcdddfb155df4984d151a7f39fceaa
Aaron <aaron.weis03@gmail.com>
2023-03-11 22:25:54 +0100

    Aufgabe 10 (10.2 ungetestet)

--- application/src/main/java/projekt/runner/RunnerImpl.java
+++ application/src/main/java/projekt/runner/RunnerImpl.java
@@ -2,6 +2,7 @@ package projekt.runner;
 
 import projekt.delivery.archetype.ProblemArchetype;
 import projekt.delivery.archetype.ProblemGroup;
+import projekt.delivery.rating.RatingCriteria;
 import projekt.delivery.service.DeliveryService;
 import projekt.delivery.simulation.BasicDeliverySimulation;
 import projekt.delivery.simulation.Simulation;
@@ -10,6 +11,9 @@ import projekt.runner.handler.ResultHandler;
 import projekt.runner.handler.SimulationFinishedHandler;
 import projekt.runner.handler.SimulationSetupHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.tudalgo.algoutils.student.Student.crash;
@@ -25,7 +29,25 @@ public class RunnerImpl implements Runner {
                     SimulationFinishedHandler simulationFinishedHandler,
                     ResultHandler resultHandler) {
 
-        crash(); // TODO: H10.2 - remove if implemented
+        List<Double> inTimeRatings = new ArrayList<>();
+        List<Double> amountDeliveredRatings = new ArrayList<>();
+        List<Double> travelDistanceRatings = new ArrayList<>();
+        Map<ProblemArchetype, Simulation> simulations = createSimulations(problemGroup, simulationConfig, deliveryServiceFactory);
+        for (int i=0; i<simulationRuns; i++) {
+            for (ProblemArchetype problem : problemGroup.problems()) {
+                Simulation simulation = simulations.get(problem);
+                simulationSetupHandler.accept(simulation,problem,i);
+                simulation.runSimulation(problem.simulationLength());
+                if (simulationFinishedHandler.accept(simulation,problem)) return;
+                inTimeRatings.add(simulation.getRatingForCriterion(RatingCriteria.IN_TIME));
+                amountDeliveredRatings.add(simulation.getRatingForCriterion(RatingCriteria.AMOUNT_DELIVERED));
+                travelDistanceRatings.add(simulation.getRatingForCriterion(RatingCriteria.TRAVEL_DISTANCE));
+            }
+        }
+        Map<RatingCriteria,Double> result = Map.of(RatingCriteria.IN_TIME,inTimeRatings.stream().mapToDouble(d -> d).average().orElse(0),
+                                                    RatingCriteria.AMOUNT_DELIVERED,amountDeliveredRatings.stream().mapToDouble(d -> d).average().orElse(0),
+                                                    RatingCriteria.TRAVEL_DISTANCE,travelDistanceRatings.stream().mapToDouble(d -> d).average().orElse(0));
+        resultHandler.accept(result);
     }
 
     @Override
@@ -33,7 +55,11 @@ public class RunnerImpl implements Runner {
                                                                 SimulationConfig simulationConfig,
                                                                 DeliveryService.Factory deliveryServiceFactory) {
 
-        return crash(); // TODO: H10.1 - remove if implemented
+        Map<ProblemArchetype, Simulation> map = new HashMap<>();
+        for (ProblemArchetype problem : problemGroup.problems()) {
+            map.put(problem, new BasicDeliverySimulation(simulationConfig,problem.raterFactoryMap(),deliveryServiceFactory.create(problem.vehicleManager()),problem.orderGeneratorFactory()));
+        }
+        return map;
     }
 
 }
